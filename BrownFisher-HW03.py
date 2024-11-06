c3511119e1778452ee94a4fb9180bef326a73e2e
fisherbrown <fbrown5@uwyo.edu>
2024-11-06 02:28:15 +0000

    push

--- BrownFisher-HW03.py
+++ BrownFisher-HW03.py
@@ -1,18 +1,31 @@
 # Fisher Brown
 # Lab Section# 10
 # 4 Nov 24
+# Worked with Jayden Robison
 
 
 
-months = {'jan' : 31, 'feb' : 28, 'mar' : 31, 'apr' : 30, 'may' : 31, 'jun' : 30, 'jul' : 31, 
-       'aug' : 31, 'sep' : 30, 'oct' : 31, 'nov' :30, 'dec': 31}
+numofdays = [31,28,31,30,31,30,31,31,30,31,30,31]
 
-months_2 = {'jan' : 01, 'feb' : 02, 'mar' : 03, 'apr' : 04, 'may' : 05, 'jun' : 06, 'jul' : 07, 
-       'aug' : 08, 'sep' : 09, 'oct' : 10, 'nov' : 11, 'dec': 12}
+days = {'sunday' : 0, 'monday' : 1, 'tuesday' : 2, 'wednesday' : 3, 'thursday' : 4, 'friday' : 5, 'saturday' : 6}
 
-long_month = ['jan','mar','may','jul','aug','oct','dec'] 
+def leap_year(year):
+    if (year % 4 == 0 and year % 100 != 0) or (year % 400 == 0):
+        numofdays[1] = 29 
+    else:
+        numofdays[1] = 28
+def first_jan(year):
+    y = year - 1
+    day_one = (36+y+(y//4)-(y//100)+(y//400))%7
+    return day_one
+def valid_date(month,day,day_one):
+    if day > numofdays[month - 1] or day <= 0:
+        print('Not a valid date')
+        return None
+    totaldays = sum(numofdays[:month - 1]) + day
+    dayofweek = (totaldays + day_one - 1) % 7  
+    return dayofweek
 
-days = {'sun' : 0, 'mon' : 1, 'tue' : 2, 'wed' : 3, 'thu' : 4, 'fri' : 5, 'sat' : 6}
 
 date = input('enter a date of the format MM/DD/YYYY: ')
 
@@ -20,29 +33,16 @@ month = int(date[0:2])
 
 day = int(date[3:5])
 
-year = int(date[6:11])
+year = int(date[6:11])        
 
-ski = date.split('/')
-
-def leap_year(year):
-    if year % 100 == 0:
-        print('not a leap year')
-    elif year % 4 == 0:
-        months['feb'] = 29
-    else: 
-        print('not a leap year')
-def first_jan(year):
-    y = year - 1
-    day_one = (36+y+(y//4)-(y//100)+(y//400))%7
-    return day_one
-def valid_date(date):
-    if ski[0] not in range(0,13):
-        print('not good enough!!!')
-    elif ski[1] = 31:
-        
 
 leap_year(year)
 day_one = first_jan(year)
+dayofweek = valid_date(month,day,day_one)
+weekday = 1
+if dayofweek is not None:
+    for key,value in days.items():
+        if dayofweek == days[key]:
+            print(f'{date} {key}')
 
-print(months)    
-print(day_one)        
+      
